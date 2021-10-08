d226cdd0b3f91bcbb631ca6a0a7176d1767ad737
Jelena <jelenbol@gmail.com>
2021-10-08 21:04:51 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+// Testing codes during the class
+
+        //  String name = "John";
+        //     System.out.println(name.charAt(1));
+
+        //       int a = 6;
+        // System.out.format("Number + %s ", a);
+
+
+        // String name = "John";
+        //         String value = String.format("My name is %s",name);
+        //System.out.println(value);
+
+
+//regular expression
+        //   String BANKiBAN = "LV10";
+        //       if (BANKiBAN.matches(regex: [A-Z]{2} [0-9]{2});
+        //      System.out.println("True");
+
+        //      if (BANKiBAN.contains)
+
+        String BANKiBAN
+        //    sout(BANKiBAN.substring(0,8))
+
+
+    }
+}
