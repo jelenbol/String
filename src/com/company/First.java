f905999abb9397f8cb27d1beb9ea9ceae725645c
Jelena <jelenbol@gmail.com>
2021-10-26 22:46:06 +0300

    First task

--- src/com/company/First.java
+++ src/com/company/First.java
@@ -66,7 +66,7 @@ public class First {
 
         for (int i = 0; i < values.length; i++)
             if (values[i].contains("te"))
-                System.out.println(+te + "contains 'te'");
+                System.out.println("Found");
 
         //it does not work properly
 
